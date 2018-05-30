ba752540183521dc2fcbe7f8c5fbaa6dd97d4bd1
Ayteko3o <37847639+Ayteko3o@users.noreply.github.com>
2018-05-31 02:13:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,14 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-const bot = new Discord.Client({disableEvreyone: true});
+client.on('ready', () => {
+    console.log('I am ready!');
+});
 
-bot.on("ready", async () => {
-  console.log("You are connected to " + bot.guilds.size + " servers!");
-  console.log(`${bot.user.username}şuan aktif!`);
-  bot.user.setGame("!!bilgi", "https://www.twitch.tv/abkc");
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 });
 
 // THIS  MUST  BE  THIS  WAY

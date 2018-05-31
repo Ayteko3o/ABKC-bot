5e8f9b17b81b6e94abb25dec5594b49b454f857f
Ayteko3o <37847639+Ayteko3o@users.noreply.github.com>
2018-05-31 11:26:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const bot = new Discord.Client({disableEvreyone: true});
+
 bot.on("ready", async () => {
   console.log("You are connected to " + bot.guilds.size + " servers!");
   console.log(`${bot.user.username}şuan aktif!`);

030fa12a1aff10c8d71b804104165c575e5b8fd5
Ayteko3o <37847639+Ayteko3o@users.noreply.github.com>
2018-06-12 01:13:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,41 +1,8 @@
-const prefix = require("./prefix.json");
 const Discord = require('discord.js');
+const client = new Discord.Client();
 
-const bot = new Discord.Client({disableEvreyone: true});
-
-bot.on('ready', async () => {
-  bot.user.setGame('deneme')
+bot.on('ready', () => {
+  bot.user.setGame('GAME HERE')
 })
 
-bot.on("message", async message => {
-  if(message.author.bot) return;
-  if(message.channel.type === "dm") return;
-
-  let prefix = abkcbot.prefix;
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-
-  if(cmd === `${prefix}bilgi`){
-
-    let sicon = message.guild.iconURL;
-    let serverembed = new Discord.RichEmbed()
-    .setDescription("ABKC sunucu hakkında")
-    .setColor("#c900e8")
-    .setThumbnail(sicon)
-    .addField("Sunucu Adı", message.guild.name)
-    .addField("Oluşturulma", message.guild.createdAt)
-    .addField("Katılma", message.member.joinedAt)
-    .addField("Üye Sayısı", message.guild.memberCount);
-
-    message.channel.send(serverembed);
-
-
-  }
-
-});
-
-
-
-// THIS  MUST  BE  THIS  WAY
-bot.login(process.env.BOT_TOKEN);
+client.login('token');

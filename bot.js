d9d518619220cc32910f45872fe47539d4deff93
Ayteko3o <37847639+Ayteko3o@users.noreply.github.com>
2018-06-12 01:27:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-bot.on('ready', () => {
-  bot.user.setGame('Hearthstone')
-})
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
 
 client.login(process.env.BOT_TOKEN);

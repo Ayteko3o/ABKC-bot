53a4ed5cee2f24c47c064c68b0e06bea73514cee
Ayteko3o <37847639+Ayteko3o@users.noreply.github.com>
2018-06-21 20:28:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const bot = new Discord.Client({disableEvreyone: true});
+
 bot.on('ready', () => {
   bot.user.setGame('deneme')
 })

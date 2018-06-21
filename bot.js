cc9abb1ff17c0e6b1f66ef6d12d73ac3e7c99fb9
Ayteko3o <37847639+Ayteko3o@users.noreply.github.com>
2018-06-21 20:25:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,8 @@
-const abkcbot = require("package.json");
-const Discord = require("index.js");
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-const bot = new Discord.Client({disableEvreyone: true});
-
-bot.on("ready", async () => {
-
-
-
+client.on('ready', () => {
+  bot.user.setGame('deneme')
+})
 
 client.login(process.env.BOT_TOKEN);

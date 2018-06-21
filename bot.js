7d91c3e7032b68aca6bd72144be1ee437c58db82
Ayteko3o <37847639+Ayteko3o@users.noreply.github.com>
2018-06-21 19:50:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,11 +3,9 @@ const client = new Discord.Client();
 
 const bot = new Discord.Client({disableEvreyone: true});
 
-bot.on("ready", async () => {
-  console.log("You are connected to " + bot.guilds.size + " servers!");
-  console.log(`${bot.user.username}şuan aktif!`);
-  bot.user.setGame("!!bilgi", "https://www.twitch.tv/abkc");
-});
+bot.on('ready', () => {
+  bot.user.setGame('Hearthstone')
+})
 
 
 client.login(process.env.BOT_TOKEN);

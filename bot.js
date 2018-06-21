2154429bf945c9dca5009ca73d57c632e5870d0f
Ayteko3o <37847639+Ayteko3o@users.noreply.github.com>
2018-06-21 20:02:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,80 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const abkcbot = require("./abkcbot.json");
+const Discord = require("discord.js");
 
 const bot = new Discord.Client({disableEvreyone: true});
 
-bot.on('ready', () => {
-  bot.user.setGame('Hearthstone')
-})
+bot.on("ready", async () => {
+  console.log("You are connected to " + bot.guilds.size + " servers!");
+  console.log(`${bot.user.username}şuan aktif!`);
+  bot.user.setGame("!!bilgi", "https://www.twitch.tv/abkc");
+});
+
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+  let prefix = abkcbot.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if(cmd === `${prefix}bilgi`){
+
+    let sicon = message.guild.iconURL;
+    let serverembed = new Discord.RichEmbed()
+    .setDescription("ABKC sunucu hakkında")
+    .setColor("#c900e8")
+    .setThumbnail(sicon)
+    .addField("Sunucu Adı", message.guild.name)
+    .addField("Oluşturulma", message.guild.createdAt)
+    .addField("Katılma", message.member.joinedAt)
+    .addField("Üye Sayısı", message.guild.memberCount);
+
+    message.channel.send(serverembed);
+
+
+  }
+
+});
+
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type ===  "dm") return;
+
+  let prefix = abkcbot.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray [0];
+  let args = messageArray.slice(1);
+
+  if(cmd === `${prefix}zaman`){
+
+    let zaman = new Date();
+    let saat = zamn.getHours();
+
+{
+alert("günaydın")
+}
+
+if (saat>=11 , saat<19)
+{
+alert("iyi öğlenler") ;  
+}
+
+if (saat>=19 , saat<23)
+{
+alert("iyi akşamlar")  
+}
+
+if(saat<6)
+{
+alert("iyi geceler")  
+}
+
+message.channel.send(serverembed);
+  
+  }
+
+});
 
 
 client.login(process.env.BOT_TOKEN);

9758fd7aef730e1c45821c9c88044131b44d8e16
Ayteko3o <37847639+Ayteko3o@users.noreply.github.com>
2018-06-21 20:35:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,7 @@ const client = new Discord.Client();
 const bot = new Discord.Client({disableEvreyone: true});
 
 bot.on("ready", async () => {
-  console.log("You are connected to " + bot.guilds.size + " servers!");
-  console.log(`${bot.user.username}şuan aktif!`);
-  bot.user.setGame("!!bilgi", "https://www.twitch.tv/abkc");
+  bot.user.setGame("!!bilgi");
 });
 
 client.login(process.env.BOT_TOKEN);

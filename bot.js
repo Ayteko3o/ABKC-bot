e0f33b54736c109051a1b21220bc38caa54158ce
Ayteko3o <37847639+Ayteko3o@users.noreply.github.com>
2018-07-25 22:37:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,11 +5,7 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
+bot.user.setPresence({ status: 'online', game: { name: 'GAME' } });
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);

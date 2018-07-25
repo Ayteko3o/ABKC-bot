cc9aaf5a5458c0063f2c44b6ff0611e0877bdc94
Ayteko3o <37847639+Ayteko3o@users.noreply.github.com>
2018-07-25 22:53:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,12 +5,6 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
-
 bot.user.setGame('GAME HERE')
 
 client.login(process.env.BOT_TOKEN);
